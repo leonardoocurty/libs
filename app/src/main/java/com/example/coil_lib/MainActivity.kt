7d9493b6fa33ddca959c86e28cd54352a7905c21
leonardoocurty <leocurty14@gmail.com>
2022-04-28 14:31:12 -0300

    Alterando propriedades do button.

--- app/src/main/java/com/example/coil_lib/MainActivity.kt
+++ app/src/main/java/com/example/coil_lib/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.coil_lib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import coil.load
 import coil.transform.CircleCropTransformation
@@ -35,6 +37,20 @@ class MainActivity : AppCompatActivity() {
 
         var imageView4 = findViewById<ImageView>(R.id.image4)
         imageView4.load("https://s2.glbimg.com/115DQucrWsNOUxf_ncmMUisprZI=/0x0:1080x819/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2020/w/a/cB6VP5QoOByFKEuCleIQ/jonreceitas-109758346-416338779271002-5424220606850697813-n.jpg"){
+            crossfade(true)
+            //placeholder(R.drawable.image)
+            transformations(CircleCropTransformation())
+        }
+
+        var imageView5 = findViewById<ImageView>(R.id.image5)
+        imageView5.load("https://www.teleculinaria.pt/wp-content/uploads/2018/09/cheesecake-de-chocolate-e-amendoa.jpg"){
+            crossfade(true)
+            //placeholder(R.drawable.image)
+            transformations(CircleCropTransformation())
+        }
+
+        var imageView6 = findViewById<ImageView>(R.id.image6)
+        imageView6.load("https://receitasdedomingo.com.br/wp-content/uploads/2019/03/Torta-de-Morango.jpg"){
             crossfade(true)
             //placeholder(R.drawable.image)
             transformations(CircleCropTransformation())
